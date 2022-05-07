3fb480de2fe4428a93ddf781bddf52a2dfb013e2
akira <asa.shakira340@gmail.com>
2022-05-08 02:44:29 +0900

    fix: get_play_count function

--- maimai_playcount/main.py
+++ maimai_playcount/main.py
@@ -5,45 +5,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chromedriver_autoinstaller.install()
 
-
-def get_play_count(sega_id, password, driver) -> int:
-    driver.get("https://maimaidx.jp/maimai-mobile/")
-
-    # enter segaid and password
-    segaid = driver.find_element(By.NAME, "segaId")
-    segaid.send_keys(sega_id)
-    password = driver.find_element(By.NAME, "password")
-    password.send_keys(password)
-    password.send_keys(Keys.RETURN)
-
-    # click aime login
-    button = driver.find_element(By.CLASS_NAME, "h_55")
-    button.click()
-
+def get_play_count(driver) -> int:
     # jump to player data page
     driver.get("https://maimaidx.jp/maimai-mobile/playerData/")
 
     # get play count
-    element = driver.find_element(By.CLASS_NAME, "f_12")
-    plays = element.get_attribute("innerHTML")
+    e = driver.find_element(By.CLASS_NAME, "f_12")
+    plays = e.get_attribute("innerHTML")
     plays = plays.split("：")
     plays = plays[1].split("回")[0]
     plays = plays.split(",")
     plays = "".join(plays)
 
-    print(int(plays))
     return int(plays)
 
 
 def main() -> None:
-    sega_id = input("SEGA ID: ")
-    password = input("PASSWORD: ")
+    chromedriver_autoinstaller.install()
+
+    # sega_id = input("SEGA ID: ")
+    # password = input("PASSWORD: ")
+    sega_id = "asakira"
+    password = "pikmin0340"
 
     driver = webdriver.Chrome()
+    driver.get("https://maimaidx.jp/maimai-mobile/")
 
-    get_play_count(sega_id, password, driver)
+    # login
+    try:
+        # enter segaid
+        e = driver.find_element(By.NAME, "segaId")
+        e.send_keys(sega_id)
+        # enter password
+        e = driver.find_element(By.NAME, "password")
+        e.send_keys(password)
+        # press enter
+        e.send_keys(Keys.RETURN)
+        # click aime login
+        button = driver.find_element(By.CLASS_NAME, "h_55")
+        button.click()
+    except:
+        print("Login Error")
+        return
+
+    play_count = get_play_count(driver)
+    print(play_count)
 
     driver.quit()
 
